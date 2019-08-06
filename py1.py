76ff60450f400726a848360eeeb34295e77a113c
jiaojiejie <31381022+jiaojiejie@users.noreply.github.com>
2019-08-06 19:14:30 +0700

    创建py1文件
    第一次用github创建项目文件

---
+++ py1.py
@@ -0,0 +1,15 @@
+_username = "jiaojiejie"
+_password = "123"
+flag = False
+for i in range(3):
+     username = input("username:")
+     password = input("password:")
+     if username == _username and password == _password:
+         print("welcome %s to login" % _username)
+         flag = True
+         break
+     else:
+        print("Invalid username or password !")
+
+if not flag:
+    print("输错三次，锁定")
